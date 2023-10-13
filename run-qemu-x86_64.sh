58f872766b7cb248565a004ba0b6de2e1a8c40db
Razvan Deaconescu <razvand@unikraft.io>
2023-10-13 18:35:47 +0300

    Add run config yaml file and setup script
    The `generate.py` script will be downloaded from
    https://raw.githubusercontent.com/unikraft/app-testing/staging/scripts/generate.py
    and used to generate the scripts in the `scripts/build/` and
    `scripts/run/` directories. This will improve maintenance (only required
    to happen for the `generate.py` script), while still keeping things
    usable by providing the basic build and run scripts.
    
    Remove old `run-...` scripts.
    
    Use the `setup.sh` script will clone all the dependencies in the
    `workdir` repository.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>
    Reviewed-by: Stefan Jumarea <stefanjumarea02@gmail.com>
    Approved-by: Stefan Jumarea <stefanjumarea02@gmail.com>
    GitHub-Closes: #10

--- run-qemu-x86_64.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-qemu-system-x86_64 -fsdev local,id=myid,path=fs0,security_model=none \
-    -device virtio-9p-pci,fsdev=myid,mount_tag=fs0 \
-    -kernel build/duktape_qemu-x86_64 \
-    -nographic
